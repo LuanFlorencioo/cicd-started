a53c1de8691227d420c8edaaa6b0aab830f29bbf
LuanFlorencio <luanflorencio.dev@gmail.com>
2025-02-02 10:46:50 -0300

    first commit

---
+++ index.mjs
@@ -0,0 +1,9 @@
+console.log(
+	`
+CLUBE DE REGATAS DO FLAMENGO
+\t- Octacampeão Brasileiro
+\t- Pentacampeão da Copa do Brasil
+\t- Tricampeão da Copa Libertadores da América
+\t- Campeão Mundial
+  `,
+);
